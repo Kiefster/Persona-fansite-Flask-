a8fb00162d8189eedf58bc8691735da594699cf8
Kiefer Vu <kiefervu@gmail.com>
2023-01-25 18:40:26 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+//function like(id) {
+  //  likes[id]++;
+  //  spans[id].innerHTML = likes[id] + "like(s)";
+  //  alert("user has liked ",likes+1,);
+//}
+  /*/if( element = document.body) {
+    var element =  element.classList.toggle("dark-mode");
+  }
+ 
+  else if (element = document.body) {
+var element =  element.classList.toggle("purple");
+  }
+  
+  else if (element = document.body) {
+    var element =  element.classList.toggle("blue");
+  }
+  
+  else{
+    var element =  element.classList.toggle("yellow");
+  }
+
+/*function myFunction() {
+  var element = document.body;
+  element.classList.toggle("purple");
+}
+function myFunction() {
+  var element = document.body;
+  element.classList.toggle("blue");
+}
+function myFunction() {
+  var element = document.body;
+  element.classList.toggle("yellow");
+  
+}*/
+console.log("page loading...")
+function removeMe(element) {   // adding removeMe function 
+  element.remove()
+}
